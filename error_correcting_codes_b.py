a114b43799753b850533946203f9d5aba7f1bba6
Sam Inzana <sinzana9@gmail.com>
2019-04-13 16:16:50 -0400

    idk why this is not working

--- error_correcting_codes_b.py
+++ error_correcting_codes_b.py
@@ -21,7 +21,7 @@ phi=np.array([[0,1,1,1],[1,1,1,0],[1,1,0,1]])
 paritybits= (np.dot(phi,bits)%2)
 codebits=np.vstack((bits,paritybits))
 print("These are the message bits:\n", bits)
-#print("This is the parity bit matrix:\n", phi)
+print("This is the parity bit matrix:\n", phi)
 print("These are the parity bits:\n",paritybits)
 print("These are the code bits:\n",codebits)
 
@@ -32,7 +32,18 @@ def bsc(txBits,p): #simulates a binary symmetric channel with transition probabi
     flips[x<p] = True
     rxBits = np.logical_xor(txBits,flips)
     return rxBits
-
+H=np.hstack((phi, np.array([[1,0,0],[0,1,0],[0,0,1]])))
+print("This is H:\n", H)
 recievedbits=(bsc(codebits,p)).astype(int)
 print("These are the recieved bits:\n", recievedbits)
+syndromebits=(np.dot(H,recievedbits)%2)
+print ("The Syndrome Bits are:\n", syndromebits)
+errorbits=(np.logical_xor(codebits,recievedbits)).astype(int)
+print("The bits in error are:\n", errorbits)
 
+shape=(7,(int(N/4)))
+biterror= (np.zeros(shape)).astype(int)
+
+for x in range (0, int(N/4)):
+    if  ((syndromebits[:,x] ==H[:,1])):
+        biterror[x,1]=1
