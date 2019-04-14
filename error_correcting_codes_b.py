785a5579a47503eb24d4f321fe8203c1b8864eee
Sam Inzana <sinzana9@gmail.com>
2019-04-14 12:23:12 -0400

    HELPPPPPPPPPPPPPPPPPPPPPPPPPPP

--- error_correcting_codes_b.py
+++ error_correcting_codes_b.py
@@ -13,13 +13,13 @@ tic=time.time()
 
 
 #N=1000032
-N=44
-bits = np.random.randint(2, size=(4, (int(N/4))))
+N=1000000
+bits = np.random.randint(2, size=(4, (int(N/4)))).astype(int)
 
-phi=np.array([[0,1,1,1],[1,1,1,0],[1,1,0,1]])
+phi=np.array([[0,1,1,1],[1,1,1,0],[1,1,0,1]]).astype(int)
 
-paritybits= (np.dot(phi,bits)%2)
-codebits=np.vstack((bits,paritybits))
+paritybits= (np.dot(phi,bits)%2).astype(int)
+codebits=np.vstack((bits,paritybits)).astype(int)
 print("These are the message bits:\n", bits)
 print("This is the parity bit matrix:\n", phi)
 print("These are the parity bits:\n",paritybits)
@@ -32,11 +32,11 @@ def bsc(txBits,p): #simulates a binary symmetric channel with transition probabi
     flips[x<p] = True
     rxBits = np.logical_xor(txBits,flips)
     return rxBits
-H=np.hstack((phi, np.array([[1,0,0],[0,1,0],[0,0,1]])))
+H=np.hstack((phi, np.array([[1,0,0],[0,1,0],[0,0,1]]))).astype(int)
 print("This is H:\n", H)
 recievedbits=(bsc(codebits,p)).astype(int)
 print("These are the recieved bits:\n", recievedbits)
-syndromebits=(np.dot(H,recievedbits)%2)
+syndromebits=(np.dot(H,recievedbits)%2).astype(int)
 print ("The Syndrome Bits are:\n", syndromebits)
 errorbits=(np.logical_xor(codebits,recievedbits)).astype(int)
 print("The bits in error are:\n", errorbits)
@@ -44,6 +44,50 @@ print("The bits in error are:\n", errorbits)
 shape=(7,(int(N/4)))
 biterror= (np.zeros(shape)).astype(int)
 
-for x in range (0, int(N/4)):
-    if  ((syndromebits[:,x] ==H[:,1])):
-        biterror[x,1]=1
+#for x in range (0,int(N/4)):
+#    if syndromebits[:,x]==H[:,0]:
+#        biterror[0,x]==1
+#    elif syndromebits[:,x]==H[:,1]:
+#        biterror[1,x]==1
+#    elif syndromebits[:,x]==H[:,2]:
+#        biterror[2,x]==1
+#    elif syndromebits[:,x]==H[:,3]:
+#        biterror[3,x]==1
+#    elif syndromebits[:,x]==H[:,4]:
+#        biterror[4,x]==1
+#    elif syndromebits[:,x]==H[:,5]:
+#        biterror[5,x]
+#    elif syndromebits[:,x]==H[:,6]:
+#        biterror[6,x]==1
+
+#for each column in the syndrome bits compare to the columns in H. where they are the same (if they are not the same dont change the message)
+#the H column will change the row of biterror[]  and the syndrome bits are the columns of biterror[]. put a 1 in the index[H,syndromebits]
+# this new matrix biterror will not equal errorbits but they should be similar
+for x in range (0,int(N/4)-1):
+    if(syndromebits[0,x]==H[0,0] and syndromebits[1,x]==H[1,0] and syndromebits[2,x]==H[2,0]):
+        biterror[0,x]=1
+    
+    elif(syndromebits[0,x]==H[0,1] and syndromebits[1,x]==H[1,1] and syndromebits[2,x]==H[2,1]):
+        biterror[1,x]=1
+        
+    elif(syndromebits[0,x]==H[0,2] and syndromebits[1,x]==H[1,2] and syndromebits[2,x]==H[2,2]):
+        biterror[2,x]=1
+        
+    elif(syndromebits[0,x]==H[0,3] and syndromebits[1,x]==H[1,3] and syndromebits[2,x]==H[2,3]):
+        biterror[3,x]=1
+        
+    elif(syndromebits[0,x]==H[0,4] and syndromebits[1,x]==H[1,4] and syndromebits[2,x]==H[2,4]):
+        biterror[4,x]=1
+        
+    elif(syndromebits[0,x]==H[0,5] and syndromebits[1,x]==H[1,5] and syndromebits[2,x]==H[2,5]):
+        biterror[5,x]=1
+        
+    elif(syndromebits[0,x]==H[0,6] and syndromebits[1,x]==H[1,6] and syndromebits[2,x]==H[2,6]):
+        biterror[6,x]=1
+print ("The new bit error:\n", biterror)
+
+correctedbits=(np.logical_xor(recievedbits,biterror)).astype(int)
+numberoferrors=np.sum(np.logical_xor(correctedbits,codebits))
+print("The corrected bits are:\n", correctedbits)
+print("The number of errors are:\n", numberoferrors)
+
